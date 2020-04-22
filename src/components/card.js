d9b6a3a8ade34218a59ebbffa0c778f60c60fcf3
rickthedev <52237593+rickthedev@users.noreply.github.com>
2020-04-23 01:01:10 +0200

    make it better

--- src/components/card.js
+++ src/components/card.js
@@ -1,12 +1,18 @@
 import React from "react"
 
+import CardStyles from "./card.module.scss"
+
 export default function Card({ image, availability, size, price }) {
   return (
-    <div>
-      <img src={image} alt={availability} />
-      <p>{availability}</p>
-      <p>{size}</p>
-      <p>{price}</p>
+    <div className={CardStyles.card}>
+      <div className={CardStyles.imgContainer}>
+        <img src={image} alt={availability} />
+      </div>
+      <div className={CardStyles.contentContainer}>
+        <p>{availability}</p>
+        <p>{size}</p>
+        <p>{price}</p>
+      </div>
     </div>
   )
 }
