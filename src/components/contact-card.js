1d7d58aba399606f8aff010a59a8782514b291e0
rickthedev <rickthedev@gmail.com>
2020-04-24 03:44:18 +0200

    contact page

--- src/components/contact-card.js
+++ src/components/contact-card.js
@@ -5,8 +5,31 @@ import ContactCardStyles from "./contact-card.module.scss"
 export default function ContactCard() {
   return (
     <div className={ContactCardStyles.contactCard}>
-      <div className={ContactCardStyles.contactInfo}></div>
-      <div className={ContactCardStyles.contactMessage}></div>
+      <div className={ContactCardStyles.contactMessage}>
+        <h3>Get in touch</h3>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquam
+          alias reprehenderit, facilis debitis voluptatem sapiente! Excepturi,
+          voluptate repellat assumenda tenetur eos labore rem ipsam suscipit
+          eius perferendis temporibus iure debitis.
+        </p>
+      </div>
+      <div className={ContactCardStyles.contactInfo}>
+        <div className={ContactCardStyles.contactContainer}>
+          <div>
+            <img src="/img/icons8-important-mail-50.png" alt="mail" />
+            <p>example@email.com</p>
+          </div>
+          <div>
+            <img src="/img/icons8-ringing-phone-50.png" alt="phone" />
+            <p>06 12 34 56 78</p>
+          </div>
+          <div>
+            <img src="/img/icons8-twitter-squared-50.png" alt="twitter" />
+            <p>@tweethandle</p>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
