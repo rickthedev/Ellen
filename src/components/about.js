7554f5b9955fde20d472bccf43da36d4f271c73b
rickthedev <rickthedev@gmail.com>
2020-06-17 19:53:51 +0200

    begin about

---
+++ src/components/about.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+import AboutStyles from "./about.module.scss"
+
+export default function About() {
+  return (
+    <div className={AboutStyles.about}>
+      <div className={AboutStyles.portrait}></div>
+
+      <h2>test</h2>
+    </div>
+  )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,13 +2,15 @@ import React from "react"
 import Layout from "../components/layout"
 import Wrapper from "../components/wrapper"
 import Header from "../components/header"
+import About from "../components/about"
 
 export default function IndexPage(props) {
   return (
     <Layout isOnIndex={true}>
       <Header></Header>
       <Wrapper>
-        <h1>About</h1>
+        {/* <h1>Over Mij</h1> */}
+        <About></About>
       </Wrapper>
     </Layout>
   )
